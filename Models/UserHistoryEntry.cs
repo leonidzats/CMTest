52a9a298ab607b0671c051042f888ffc653291bf
Leonid <leonidzats@gmail.com>
2019-09-14 18:29:40 +0300

    local time to string

--- Models/UserHistoryEntry.cs
+++ Models/UserHistoryEntry.cs
@@ -9,24 +9,24 @@ namespace Netflix.Models
         public string Title{get;private set;}
         public string UserRank{get;private set;}
         public string ImdbRank{get;private set;}
-        public DateTime EntryDate {get;private set;}
+        public string EntryDate {get;private set;}
 
         public void UpdateUserRank(float new_rank){
             this.UserRank = new_rank.ToString(".0");
         }
 
-        public UserHistoryEntry(string id,string title,string ImdbRank,float userRank=0)
+        public UserHistoryEntry(string id,string title,string ImdbRank,float userRank=0,string EntryDate=null)
         {
             this.Id = id;
             this.Title = title;
             this.ImdbRank = ImdbRank;
-            this.EntryDate = DateTime.Now;
+            this.EntryDate = EntryDate ?? DateTime.Now.ToString();
             this.UserRank = userRank.ToString(".0");
         }
 
         public override string ToString(){
             return string.Format("watched at - {0} , title - {1} , user rank - {2} , imdb rank - {3}",
-                            this.EntryDate.ToString("dddd, dd MMMM yyyy HH:mm"),
+                            DateTime.Parse(this.EntryDate).ToString("dddd, dd MMMM yyyy HH:mm"),
                             this.Title,
                             this.UserRank,
                             string.IsNullOrEmpty(this.ImdbRank)? 
